5db380652fed782c5824de432bd871be91f6c143
vijaynaidu013 <84774714+vijaynaidu013@users.noreply.github.com>
2021-09-09 09:06:05 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
-def home(): 
+
+def home()  
+  
 return "Hello World......!!!!!"
+
 if __name__ == '__main__':
 app.run(threaded=True,host='0.0.0.0',port=5000)
